66b9ebd08a5c2c30700e94bbfa0b85ff89356108
lachlan <lockym969@gmail.com>
2025-09-24 13:40:37 +1000

    Commit

---
+++ prac_02/score.py
@@ -0,0 +1,16 @@
+
+def main():
+    score = float(input("Enter score: "))
+    print(rate_score(score))
+
+def rate_score(score):
+    if score < 0 or score > 100:
+        return("Invalid score")
+    elif score >= 90:
+        return ("Excellent")
+    elif score >= 50:
+        return ("Passable")
+    else:
+        return ("Bad")
+
+main()
