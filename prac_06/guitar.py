c83529ea786b43a9493e91b485c3e78c5b629b8f
Lachlan-magill <lockym969@gmail.com>
2025-11-01 20:54:46 +1000

    Create guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,19 @@
+"""estimated time 10 min
+started at 10:14am
+finished at 10:33am
+total time 19min"""
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}): ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        current_year = 2024
+        return current_year - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
